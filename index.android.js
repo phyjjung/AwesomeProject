a2e54f118bd222bef80f4d67ca55e8585d82f48b
phyjjung <junghoon.bak@gmail.com>
2016-10-16 22:50:41 +0900

    index.andorid.js 변경
    강의와 같은 포맷으로 변경함. 근데 강의와 좀 달라진 면이 있음.

--- index.android.js
+++ index.android.js
@@ -4,7 +4,8 @@
  * @flow
  *ㅇ요거 추가해서 함 보자고.ㄴ
  */
-
+'use strick';
+//강의에서는 'react-native'에서 모두 가지고 온다.
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -13,19 +14,12 @@ import {
   View
 } from 'react-native';
 
-export default class AwesomeProject extends Component {
+class AwesomeProject extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
+        <Text>
           Welcome to React Native! gogogog!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
         </Text>
       </View>
     );
@@ -35,20 +29,13 @@ export default class AwesomeProject extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    position: "absolute",
+    left: 0,
+    right: 0,
+    bottom: 0,
+    top: 0
   },
 });
 
+// 강의에서는 react.AppRegistry 인데 이게 먹히지 않고 아래와 같이 해야함.
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
